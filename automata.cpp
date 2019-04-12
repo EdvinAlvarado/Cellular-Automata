70843566cc2bf8f587b98276304f9523ae6c2623
EdvinAlvarado <ealvarado301@gmail.com>
2019-04-12 10:46:21 -0400

    Change the metric to wet dirt per grid and established structs so it wouldn't need to do various iterations per grid stat.

--- automata.cpp
+++ automata.cpp
@@ -28,20 +28,6 @@ void printArray(array<array<int,rowsize>, colsize> &matrix) {
 	}
 }
 
-void prettyPrintArray(array<array<int,rowsize>, colsize> &matrix) {
-	for (int y = 0; y < rowsize; y++) {
-		for (int x = 0; x < colsize; x++) {
-			if (matrix[y][x] == 1) {
-				cout << "o";
-			} else if (matrix[y][x] == 0) {
-				cout << "-";
-			}
-			cout << " "; 
-		}
-		cout << endl;
-	}
-}
-
 int adjacent(array<array<int,rowsize>, colsize> &matrix, int row, int col) {
 	int adj_val;
 	if (row == 0) {
@@ -116,30 +102,85 @@ int diagonal_adjacent(array<array<int,rowsize>, colsize> &matrix, int row, int c
 	return adj_val;
 }
 
-int nonzero (array<array<int,rowsize>, colsize> &matrix) {
-	int sum;
+void prettyPrintArray(array<array<int,rowsize>, colsize> &matrix) {
 	for (int y = 0; y < rowsize; y++) {
 		for (int x = 0; x < colsize; x++) {
-			sum += matrix[y][x];
+			if (matrix[y][x] == 1) {
+				cout << "@";
+			} else if (matrix[y][x] == 0) {
+				if (adjacent(matrix, y, x) == 0) {
+					cout << "~";
+				} else {
+					cout << "-";
+				}				
+			}
+			cout << " "; 
+		}
+		cout << endl;
+	}
+}
+
+// all the water.
+// int nonzero (array<array<int,rowsize>, colsize> &matrix) {
+// 	int sum = 0;
+// 	for (int y = 0; y < rowsize; y++) {
+// 		for (int x = 0; x < colsize; x++) {
+// 			sum += matrix[y][x];
+// 		}
+// 	}
+// 	return sum;
+// } 
+
+// int drydirt (array<array<int,rowsize>, colsize> &matrix) {
+// 	int drydirt_num = 0;
+// 	for (int y = 0; y < rowsize; y++) {
+// 		for (int x = 0; x < colsize; x++) {
+// 			if ((adjacent(matrix, y, x) == 0) && (matrix[y][x] == 0)) {
+// 				drydirt_num += 1;
+// 			}
+// 		}
+// 	}
+// 	return drydirt_num;
+// }
+
+struct grid_data_struct {
+	int drydirt;
+	int water;
+	int wetdirt;
+};
+
+grid_data_struct grid_data (array<array<int,rowsize>, colsize> &matrix) {
+	int drydirt_num = 0;
+	int water_num = 0;
+	int wetdirt_num = 0;
+	for (int y = 0; y < rowsize; y++) {
+		for (int x = 0; x < colsize; x++) {
+			water_num += matrix[y][x];
+			if ((adjacent(matrix, y, x) == 0) && (matrix[y][x] == 0)) {
+				drydirt_num += 1;
+			}
+			if ((adjacent(matrix, y, x) > 0) && (matrix[y][x] == 0)) {
+				wetdirt_num += 1;
+			}
 		}
 	}
-	return sum;
-} 
+	return {drydirt_num, water_num, wetdirt_num};
+}
 
 int main() {
 	srand(time(NULL)); // set rand seed
 	array<array<int,rowsize>, colsize> grid; // initialize 2D array
 	float max_ratio = 0; 
-	for (int runs = 0; runs < 30; runs++) { // sets different initial conditions
+	for (int runs = 0; runs < 50; runs++) { // sets different initial conditions
 		randintMatrix(grid);
 		cout << "before" << endl;
 		prettyPrintArray(grid); 
-		for (int iterations = 0; iterations < 10000; iterations++) { // iterations in each condition.
+		for (int iterations = 0; iterations < 2000; iterations++) { // iterations in each condition.
 			for (int y = 0; y < rowsize; y++) {
 				for (int x = 0; x < colsize; x++) {
 					if ((grid[y][x] == 0) && (adjacent(grid, y, x) == 0)) {
 						grid[y][x] = 1;
-					} else if ((grid[y][x] == 1) && (diagonal_adjacent(grid, y, x) > 0)) {
+					} else if ((grid[y][x] == 1) && (adjacent(grid, y, x) > 0)) {
 						grid[y][x] = 0;
 					}
 				}
@@ -148,12 +189,16 @@ int main() {
 		cout << "after" << endl;
 		prettyPrintArray(grid); 		
 
+		auto gridstats = grid_data(grid);
 		float gridsize = rowsize * colsize;
-		float ratio = gridsize / nonzero(grid);
-		cout << "ratio: " << ratio << endl;
-		max_ratio = max(max_ratio, ratio);
+		// float wetdirt = gridsize - gridstats.drydirt - gridstats.water;
+		float dirtper = gridstats.wetdirt / gridsize;
+		// cout << "ratio: " << ratio << endl;
+		cout << "wet dirt % " << dirtper * 100<< "%" << endl;
+		max_ratio = max(max_ratio, dirtper);
 		cout << endl;
 	} // different initial conditions.
-	cout << "max grid:water ratio " << max_ratio << endl;
-	cout << "dirt percentage: " << (max_ratio - 1) / max_ratio * 100 << "%" << endl;
+	// cout << "max grid:water ratio " << max_ratio << endl;
+	cout << "wet dirt percentage:  " << max_ratio * 100 << "%" << endl;
+	// cout << "dirt percentage: " << (max_ratio - 1) / max_ratio * 100 << "%" << endl;
 }
