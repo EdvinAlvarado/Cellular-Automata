df744535d0d41f57021f80c0e8364a3bf76ebdb4
EdvinAlvarado <ealvarado301@gmail.com>
2019-05-31 14:49:47 -0400

    faster and allegedly more random.

--- cellular_automaton_ncurses.cpp
+++ cellular_automaton_ncurses.cpp
@@ -6,13 +6,14 @@
 #include <chrono>
 #include <thread>
 #include <stdlib.h> //system access
-// #include <curses.h>
+#include <curses.h>
 #include <ncurses.h>
+#include <random>
 
 using namespace std;
 //global values
 const int rowsize = 60;	
-const int colsize = 60;
+const int colsize = 160;
 
 // void clrscr() {
 // 	cout << "\033[2J\033[1;1H";
@@ -20,9 +21,14 @@ const int colsize = 60;
 
 
 void randintMatrix(array<array<int,rowsize>, colsize> &matrix) {
+	// std::srand(std::time(nullptr)); // rand based on time
+	// https://en.cppreference.com/w/cpp/numeric/random/uniform_int_distribution
+	std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::uniform_int_distribution<> dis(0, 1);
 	for (int y = 0; y < rowsize; y++) {
 		for (int x = 0; x < colsize; x++) {
-			matrix[y][x] = rand() % 2; // 0 or 1
+			matrix[y][x] = dis(gen); // 0 or 1
 		}
 	}
 }
@@ -110,6 +116,7 @@ int diagonal_adjacent(array<array<int,rowsize>, colsize> &matrix, int row, int c
 }
 
 void AnimationPrint(array<array<int,rowsize>, colsize> &matrix, WINDOW * windows) {
+	wborder(windows, 0, 0, 0, 0, 0, 0, 0, 0);
 	for (int y = 0; y < rowsize; y++) {
 		for (int x = 0; x < colsize; x++) {
 			if (matrix[y][x] == 1) {
@@ -121,16 +128,16 @@ void AnimationPrint(array<array<int,rowsize>, colsize> &matrix, WINDOW * windows
 	}
 	wmove(windows, 0, 0);
 	wrefresh(windows);
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
 
 int main() {
-	srand(time(NULL)); // set rand seed
 	array<array<int, rowsize>, colsize> grid; // initialize 2D array
 	array<array<int, rowsize>, colsize> change_grid = {};
 	initscr(); // Needed to use any curses packages
 	WINDOW * win = newwin(rowsize, colsize, 0, 0); // create the window object 
-	for (int runs = 0; runs < 1; runs++) { // sets different initial conditions
+	for (int runs = 0; runs < 10; runs++) { // sets different initial conditions
+		// srand(time(NULL)); // set rand seed
 		randintMatrix(grid);
 		// zerofillMatrix(change_grid); // zero fill is broken. gives random numbers.
 		// cout << "before" << endl;
@@ -149,7 +156,7 @@ int main() {
 						} else {
 							change_grid[y][x] = 1; // lives on
 						}
-					} else if (diagonal_adjacent(grid, y, x) == 3) { //implicit grid value to be zero.
+					} else if (diagonal_adjacent(grid, y, x) == 3) { //implicit grid value is zero.
 						change_grid[y][x] = 1; // reproduction
 					} else {
 						change_grid[y][x] = 0;
@@ -157,18 +164,9 @@ int main() {
 				}
 			}
 			copyMatrix(change_grid, grid);
-			// cout << endl;
-			// cout << "\n\n\n\n\n\n\n\n\n\n\n\n";
-			// system("CLS");
-			// this_thread::sleep_for(chrono::milliseconds(100));
-			// prettyPrintArray(grid); 
 			AnimationPrint(grid, win); 
 		} // different iteration.
-		// cout << "after" << endl;
-		// prettyPrintArray(grid); 
-
+		std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 	} // different initial conditions.
-	// system("pause");
-	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 	endwin();
 }
